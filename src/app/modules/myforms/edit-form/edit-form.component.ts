102b6198e56fcb536bb7f1351180c145f26b137d
Rifat Cholakov <contact@rifatcholakov.com>
2020-02-17 00:33:11 +0200

    editform created

--- src/app/modules/myforms/edit-form/edit-form.component.ts
+++ src/app/modules/myforms/edit-form/edit-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ToolbarService, LinkService, ImageService, HtmlEditorService } from '@syncfusion/ej2-angular-richtexteditor';
 
 @Component({
   selector: 'app-edit-form',
   templateUrl: './edit-form.component.html',
-  styleUrls: ['./edit-form.component.css']
+  styleUrls: ['./edit-form.component.css'],
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService]
 })
 export class EditFormComponent implements OnInit {
 
--- src/app/modules/myforms/myforms.module.ts
+++ src/app/modules/myforms/myforms.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditFormComponent } from './edit-form/edit-form.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { RichTextEditorModule } from '@syncfusion/ej2-angular-richtexteditor';
 
 
 @NgModule({
   declarations: [EditFormComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RichTextEditorModule
   ],
   exports: [
     EditFormComponent

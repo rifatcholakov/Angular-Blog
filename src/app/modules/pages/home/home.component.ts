2fa5f69734877161e61989449152a208694c6c8e
Rifat Cholakov <contact@rifatcholakov.com>
2020-02-18 23:27:23 +0200

    Added posts to home page

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/shared/interfaces/post';
+import { Subscription } from 'rxjs';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  posts: Array<Post>;
+  getPostsSubscription: Subscription;
+
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.getPostsSubscription = this.api.getPosts().subscribe(
+      (posts: Array<Post>) => this.posts = posts
+                                            .sort((a, b) => { 
+                                                  return b.createdAt - a.createdAt 
+                                            })
+                                            .slice(0, 3)
+    )
+  }
+
+  ngOnDestroy() {
+    this.getPostsSubscription.unsubscribe();
   }
 
 }

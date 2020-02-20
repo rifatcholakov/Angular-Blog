db8d444f870c3bfaedbe9d95449eb029c0b4923f
Rifat Cholakov <contact@rifatcholakov.com>
2020-02-21 00:38:34 +0200

    Updated for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCTDMFOX84zxPujGVleMGPkzM-b4C36jDo",
+    authDomain: "blog-3f7e4.firebaseapp.com",
+    databaseURL: "https://blog-3f7e4.firebaseio.com",
+    projectId: "blog-3f7e4",
+    storageBucket: "blog-3f7e4.appspot.com",
+    messagingSenderId: "79128405700",
+    appId: "1:79128405700:web:f20c22649e3095b637cb91"
+  }
 };

4b8abeee59704b7f4dabddeb7aaed7de0c6e1b0e
Rifat Cholakov <contact@rifatcholakov.com>
2020-02-20 13:22:21 +0200

    Added create new post button

--- src/app/modules/pages/blog/blog.component.ts
+++ src/app/modules/pages/blog/blog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from 'src/app/core/services/api.service';
 import { Post } from 'src/app/shared/interfaces/post';
 import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Component({
   selector: 'app-blog',
@@ -12,7 +13,7 @@ export class BlogComponent implements OnInit, OnDestroy {
   posts: Array<Post>;
   getPostsSubscription: Subscription;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, public auth: AuthenticationService) { }
 
   ngOnInit() {
     this.getPostsSubscription = this.api.getPosts().subscribe(
